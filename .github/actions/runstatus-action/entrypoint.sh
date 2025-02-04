61f19c7773ddac30aac175a6f21f7fe5137435a3
Eitan Lieberman <92268762+eitanLieberman@users.noreply.github.com>
2025-02-04 07:27:27 +0200

    rm echoes of sensitive info

--- .github/actions/runstatus-action/entrypoint.sh
+++ .github/actions/runstatus-action/entrypoint.sh
@@ -5,14 +5,11 @@ set -e
 # Capture inputs as environment variables
 echo "Executing GitHub Repo Status Notifier..."
 
-echo "GITHUB_TOKEN: $GITHUB_TOKEN"
 echo "REPO_NAME: $REPO_NAME"
 echo "REPO_OWNER: $REPO_OWNER"
-echo "SLACK_USER_TOKEN: $SLACK_USER_TOKEN"
 echo "LOOKBACK_DAYS: $LOOKBACK_DAYS"
 echo "SLACK_TEAM_ID: $SLACK_TEAM_ID"
-echo "SLACK_WEBHOOK_URL: $SLACK_WEBHOOK_URL"
-echo "RECIPIENTS: $RECIPIENTS"  # This should now print the value of the recipients
+echo "RECIPIENTS: $RECIPIENTS"
 
 curl -X POST \
   -H "Content-Type: application/json" \
